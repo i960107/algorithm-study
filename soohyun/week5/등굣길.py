af302ecaf18b6bfb3cad303950d3e490393beaad
i960107 <i960107@naver.com>
2023-02-23 16:32:42 +0900

    Add Offline Study DP Solutions

---
+++ soohyun/week5/등굣길.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+def solution(m: int, n: int, puddles: List[List[int]]) -> int:
+    dp = [[0] * m for _ in range(n)]
+    passable = [[True] * m for _ in range(n)]
+    for x, y in puddles:
+        passable[y - 1][x - 1] = False
+
+    for c in range(m):
+        if not passable[0][c]:
+            break
+        dp[0][c] = 1
+
+    for r in range(n):
+        if not passable[r][0]:
+            break
+        dp[r][0] = 1
+
+    for r in range(1, n):
+        for c in range(1, m):
+            if not passable[r][c]:
+                dp[r][c] = 0
+                continue
+            dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+    print(dp)
+    return dp[n - 1][m - 1] % 1000000007
+
+
+# 물에 잠긴 지역은 최대 10개 이므로 리스트 선형 탐색 괜찮음?
+# 매번 건널때마다 10번씩 탐색해야하는 것은 비효율적
+# 미리 배열 만들어두기
+# 주의! puddles는 1부터 시작하는 좌표를 나타냄
+# def is_sunk(r: int, c: int, puddles: List[List[int]]) -> bool:
+#     for x, y in puddles:
+#         if x == c + 1 and y == r + 1:
+#             return True
+#     return False
+
+
+print(solution(4, 3, [[2, 2]]))
--- soohyun/week5/퇴사2.py
+++ soohyun/week5/퇴사2.py
@@ -10,8 +10,19 @@ def solution(n: int, pay: List[Tuple[int, int]]) -> int:
         if day + t <= n + 1:
             dp[day + t] = max(dp[day + t], dp[day] + p)
 
-    # 왜 max를 해줘야하는지 조금 헷갈림.
+    # 전날까지 벌 수 있는 최대 금액을 기록했고, 마지막 인덱스의 경우 최대값으로 갱신이 안되므로 비교 필요
     return max(dp[n], dp[n + 1])
+
+
+def solution_best(n: int, pay: List[Tuple[int, int]]) -> int:
+    # 그날까지 벌 수 있는 최대 금액
+    dp = [0] * (n + 1)
+    for day, (t, p) in enumerate(pay, 1):
+        dp[day] = max(dp[day - 1], dp[day])
+        if day + t - 1 <= n:
+            dp[day + t - 1] = max(dp[day + t - 1], dp[day - 1] + p)
+
+    return dp[n]
 
 
 n = int(input())
@@ -20,4 +31,4 @@ input = stdin.readline
 for _ in range(n):
     t, p = map(int, input().split())
     pay.append((t, p))
-print(solution(n, pay))
+print(solution_best(n, pay))
