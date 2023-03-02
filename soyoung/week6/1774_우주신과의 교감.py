93b59f0318bcff432ec2f704f1aed24325034a9f
sosinny <psoyoung0302@gmail.com>
2023-03-02 16:52:27 +0900

    Add offline solutions week6(친구네트워크)

--- soyoung/week6/1774_우주신과의 교감.py
+++ soyoung/week6/1774_우주신과의 교감.py
@@ -14,23 +14,25 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-n = int(sys.stdin.readline())
+n, m = map(int, sys.stdin.readline().split())
 parent = [0] * (n+1)
 for i in range(1, n+1):
     parent[i] = i
-stars = []  # 별의 좌표
+points = []  # 우주인의 좌표
 edges = []  # 간선 정보
 ans = 0
 for i in range(n):
-    x, y = map(float, sys.stdin.readline().split())
-    stars.append((x, y))
-
+    x, y = map(int, sys.stdin.readline().split())
+    points.append((x, y))
+idx1, idx2 = map(int, sys.stdin.readline().split())
+union_parent(parent, idx1, idx2)
+# edges.append((math.sqrt((points[idx1-1][0] - points[idx2-1][0])**2 + (points[idx1-1][1] - points[idx2-1][1])**2), idx1-1, idx2-1))
 for i in range(n-1):
     for j in range(i+1, n):
         # 간선에 (cost, i, j) 붙이기
-        edges.append((math.sqrt((stars[i][0] - stars[j][0])**2 + (stars[i][1] - stars[j][1])**2), i, j))
+        edges.append((math.sqrt((points[i][0] - points[j][0])**2 + (points[i][1] - points[j][1])**2), i, j))
 edges.sort()
-
+print(edges)
 for i in range(len(edges)):
     cost, a, b = edges[i]
     if find_parent(parent, a) != find_parent(parent, b):
