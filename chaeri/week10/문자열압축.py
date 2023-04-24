39a2ec85da2c78992c7fd94c15f859219615cce3
chaeri kim <kcr7812@naver.com>
2023-04-24 14:04:37 +0900

    0424

---
+++ chaeri/week10/문자열압축.py
@@ -0,0 +1,26 @@
+def solution(s):
+    ans = len(s) 
+
+    for a in range(1, len(s)//2+1): 
+        result = "" 
+        slice = s[0:a] 
+        cnt = 1 
+        for i in range(a, len(s), a):
+            if slice == s[i: i+a]:
+                cnt += 1 
+            else:
+                if cnt == 1: 
+                    result += slice 
+                    slice = s[i: i+a] 
+                else: 
+                    result += (str(cnt) + slice)
+                    cnt = 1 
+                    slice = s[i: i+a] 
+        
+        if cnt == 1: 
+            result += slice
+        else: result += (str(cnt) + slice)
+            
+        ans = min(ans, len(result))
+    
+    return ans
